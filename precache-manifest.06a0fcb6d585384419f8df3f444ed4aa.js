cea839e05c7f2a28bd6e55a1531b63b649aba670
Unknown65182 <unknown65182@gmail.com>
2020-07-21 18:32:51 +0300

    Updates

---
+++ precache-manifest.06a0fcb6d585384419f8df3f444ed4aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970b5fe41cbbaf9e42e20bf4f94757ae",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "522dc05cca40fe660946",
+    "url": "/todo/static/js/2.a9b2c925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.a9b2c925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51586c6e863dc8edb59",
+    "url": "/todo/static/js/main.ba5a4012.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
